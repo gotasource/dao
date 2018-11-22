ea76913aeb8e33f1bb425846c66ed77c185c8743
gotasource <gotasource@gmail.com>
2018-11-22 23:58:04 +0700

    apply behavior filter move common method to @gota/core update dynamic generic service

---
+++ lib/decorator.ts
@@ -0,0 +1,105 @@
+
+//https://rbuckton.github.io/reflect-metadata/#syntax
+import 'reflect-metadata'
+import { Helper } from '@gota/core';
+
+const DESIGN_META_DATA = {
+    APP : 'design:meta:data:key:app',
+    CONFIG : 'design:meta:data:key:config',
+    POST_INIT : 'design:meta:data:key:post.init',
+    AUTOWIRED : 'design:meta:data:key:autowired',
+    SERVICE : 'design:meta:data:key:service',
+    SERVICE_MAPPING : 'design:meta:data:key:service:mapping',
+    PATH : 'design:meta:data:key:path',
+    METHOD : 'design:meta:data:key:method',
+    PARAMETER : 'design:meta:data:key:parameter',
+    PATH_PARAMETER : 'design:meta:data:key:path:parameter',
+    REQUEST : 'design:meta:data:key:request',
+    RESPONSE : 'design:meta:data:key:response',
+    QUERY : 'design:meta:data:key:query',
+    QUERY_PARAMETER : 'design:meta:data:key:query:parameter',
+    BODY : 'design:meta:data:key:body',
+    BODY_PARAMETER : 'design:meta:data:key:body:parameter',
+    HEADERS : 'design:meta:data:key:headers',
+    HEADERS_PARAMETER : 'design:meta:data:key:headers:parameter',
+    ENTITY : 'design:meta:data:key:entity',
+    ENTITY_CONTAINER : 'design:meta:data:key:entity:container',
+    FIELD_DYNAMIC_ACCESS_MODE : 'design:meta:data:key:field:dynamic:access:mode'
+};
+
+const REQUEST_METHOD = {
+    OPTIONS: 'OPTIONS',
+    GET :'GET',
+    POST :'POST',//CREATE
+    PUT :'PUT',// REPLACE
+    PATCH : 'PATCH',// UPDATE
+    DELETE : 'DELETE'
+};
+//export class RequestMethod{
+//    static OPTIONS = 'OPTIONS';
+//    static GET  =  'GET';
+//    static  POST  = 'POST';
+//    static  PUT  = 'PUT';
+//    static  PATCH  = 'PATCH';
+//    static  DELETE = 'DELETE';
+//}
+
+export class EntityContainer{
+    private static entities: Array<Function> = [];
+    public static addEntity(entity: Function){
+        this.entities.push(entity);
+    }
+    public static getEntities():Array<Function>{
+        return this.entities;
+    }
+    public static findEntity(name:String): Function{
+        return this.entities.find(item => item.name === name);
+    }
+}
+
+export function Entity(properties?: Array<{name:string, type: Function, dynamicAccessMode?: Array<String>}>){
+	return function(... args : any[]): void {
+        let clazz = args[0];
+        properties.forEach(property =>{
+            let dynamicAccessModes = Reflect.getMetadata(DESIGN_META_DATA.FIELD_DYNAMIC_ACCESS_MODE, clazz.prototype, property.name);
+            if(Array.isArray(dynamicAccessModes)){
+                property.dynamicAccessMode = dynamicAccessModes;
+            }
+        });
+        let superClazz = Helper.findSuper(args[0]);
+        let superProperties = (Helper.findDeclaredProperties(superClazz)||[]).filter(item => properties.findIndex(i => i.name !== item.name));
+        let propertiesPlus =  [... superProperties, ... properties];
+	    Reflect.defineMetadata(DESIGN_META_DATA.ENTITY, propertiesPlus, args[0]);
+        EntityContainer.addEntity(args[0]);
+	}
+}
+
+export function Field(){
+    return function(... args : any[]): void {
+        let clazz = Helper.findSuper(args[0]);
+        let propertiesPlus =  [... Helper.findDeclaredProperties(clazz)];
+        Reflect.defineMetadata(DESIGN_META_DATA.ENTITY, propertiesPlus, args[0]);
+    }
+}
+
+export class DynamicAccessMode{
+    static READ  = 'READ';
+    static WRITE = 'WRITE';
+}
+
+export function DynamicAccess(modes: String | Array<String>){
+    let _modes:Array<String>;
+    if(!Array.isArray(modes)){
+        _modes = [modes as String];
+    }else{
+        _modes = modes as Array<String>;
+    }
+
+    return function(... args : any[]): void {
+        let prototype = args[0];
+        let property = args[1];
+        Reflect.defineMetadata(DESIGN_META_DATA.FIELD_DYNAMIC_ACCESS_MODE, _modes, prototype, property);
+    }
+}
+
+
