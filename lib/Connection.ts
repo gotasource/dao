d068d58c0080267e9bc333e75708cc46d9195d10
gotasource <gotasource@gmail.com>
2018-11-24 15:31:12 +0700

    update Connection config

--- lib/Connection.ts
+++ lib/Connection.ts
@@ -1,4 +1,4 @@
- import {Server, Db, Collection, MongoClient} from 'mongodb';
+ import {Server, Db, Collection, MongoClient, MongoClientOptions} from 'mongodb';
  import {Config} from '@gota/injection';
  import {PostInit} from '@gota/core';
 
@@ -20,21 +20,29 @@ class Connection {
                     let databaseFormat =
                     `
                     database: {
+                        protocol: <protocol>
                         host: <host>,
                         port: <port>,
                         databaseName:<databaseName>
                     }
                     or
                      database: {
-                        url: <url>
+                        url: <url>,
+                        databaseName:<databaseName>
                     }
                     `;
                     throw new Error('\n\rDatabase config is not found or wrong format. please update database config with format: ' + databaseFormat);
                 }
             let databaseName = this.databaseConfig.databaseName;
-            let url = this.databaseConfig.url || `mongodb://${this.databaseConfig.host}:${this.databaseConfig.port}/`;
+            let url = this.databaseConfig.url 
+                || `${this.databaseConfig.protocol}://${this.databaseConfig.user?this.databaseConfig.user+':':''}${this.databaseConfig.password?this.databaseConfig.password+'@':''}${this.databaseConfig.host}${this.databaseConfig.port?':'+this.databaseConfig.port:''}`;
+           
             try{
-                this.mongoClient = await MongoClient.connect(url, { useNewUrlParser: true });
+                let mongoClientOptions: MongoClientOptions = {};
+                mongoClientOptions.useNewUrlParser = true;
+                mongoClientOptions = Object.assign(mongoClientOptions, this.databaseConfig.options);
+
+                this.mongoClient = await MongoClient.connect(url, mongoClientOptions);
                 Connection.db = this.mongoClient.db(databaseName);
             }catch (err){
                 console.log("Create Connection is Failed.\nPlease check database config: \n" + JSON.stringify(this.databaseConfig, null, 4));
